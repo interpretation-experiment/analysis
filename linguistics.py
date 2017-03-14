384610358a9a96451b6f12479d830b39c810e0a2
Sébastien Lerique <sl@mehho.net>
2017-03-14 17:06:57 +0100

    feat/linguistics: detect doubleposts

--- linguistics.py
+++ linguistics.py
@@ -4,6 +4,7 @@ from warnings import warn
 
 from django.conf import settings
 from django.db.models.manager import Manager
+from django.db.models.query import QuerySet
 from nltk.corpus import stopwords as nltk_stopwords
 from nltk.metrics import jaccard_distance, edit_distance
 from nltk.stem.snowball import EnglishStemmer as SnowballStemmer
@@ -241,12 +242,14 @@ def equip_sentence_codings(models):
     """Import external codings on sentences.
 
     Codings imported:
-    * `spam` (with `spam_detail`, and `ham` on `SentenceManager`)
-    * `rogue`
+    * `spam` (with `spam_detail`, and `nonspam` on `SentenceManager`
+      and `QuerySet`)
+    * `doublepost` (with `nondoublepost` on `SentenceManager` and `QuerySet`)
+    * `rogue` (with `nonrogue` on `SentenceManager` and `QuerySet`)
 
     Also add Sentence.LOADED_CODINGS which lists loaded codings, and `kept`
-    and `with_dropped()` on `SentenceManager` which filters on sentences kept
-    after deciding on all codings.
+    and `with_dropped()` on `SentenceManager` and `QuerySet` which filters on
+    sentences kept after deciding on all codings.
 
     """
 
@@ -262,60 +265,123 @@ def equip_sentence_codings(models):
     # Add them as properties to Sentence
     models.Sentence._spam_codings = spam_codings
     models.Sentence.spam_detail = property(memoized(
-            lambda self: self._spam_codings.get(self.id, [(False, None)])))
-    models.Sentence.self_spam = property(memoized(
-            lambda self: np.mean([spam for (spam, _)
-                                  in self.spam_detail]) > 0.5))
+        lambda self: self._spam_codings.get(self.id, [(False, None)])
+    ))
+    models.Sentence._self_spam = property(memoized(
+        lambda self: np.mean([spam for (spam, _) in self.spam_detail]) > 0.5
+    ))
     models.Sentence.spam = property(memoized(
-            lambda self: self.self_spam or (self.parent is not None and
-                                            self.parent.spam)))
+        lambda self: (self._self_spam or
+                      (self.parent is not None and self.parent.spam))
+    ))
 
     # Give easy access to non-spam sentences
     @memoized
-    def get_ham(self):
+    def get_nonspam(self):
         if self.model != models.Sentence:
             raise ValueError('Only available on Sentence')
 
-        qs = self.get_queryset()
-        ids = [s.id for s in qs if not s.spam]
-        return qs.filter(pk__in=ids)
+        ids = [s.id for s in self.all() if not s.spam]
+        return self.filter(pk__in=ids)
 
-    Manager.ham = property(get_ham)
+    Manager.nonspam = property(get_nonspam)
+    QuerySet.nonspam = property(get_nonspam)
+
+    # Create doublepost codings
+    @memoized
+    def get_self_doublepost(self):
+        if self.parent is None:
+            # Root sentences are not doubleposts
+            return False
+
+        if self.profile.user.is_staff:
+            # Staff-made sentences are not self_doublepost (though they
+            # can descend from a self_doublepost)
+            return False
+
+        # So we're from a non-staff profile. Are we a self_doublepost?
+        others = self.tree.sentences\
+            .filter(profile=self.profile)\
+            .exclude(id=self.id)\
+            .order_by('id')
+        # If there is another sentence by this same (non-staff) profile,
+        # with smaller id, we're self_doublepost.
+        return (others.count() > 0 and others.first().id < self.id)
+
+    models.Sentence._self_doublepost = property(get_self_doublepost)
+    models.Sentence.doublepost = property(memoized(
+        lambda self: (self._self_doublepost or
+                      (self.parent is not None and self.parent.doublepost))
+    ))
+
+    # Give easy access to non-doublepost sentences
+    @memoized
+    def get_nondoublepost(self):
+        if self.model != models.Sentence:
+            raise ValueError('Only available on Sentence')
+
+        ids = [s.id for s in self.all() if not s.doublepost]
+        return self.filter(pk__in=ids)
+
+    Manager.nondoublepost = property(get_nondoublepost)
+    QuerySet.nondoublepost = property(get_nondoublepost)
 
     # Create rogue codings
     @memoized
-    def get_rogue(self):
+    def get_self_rogue(self):
         if self.parent is None:
+            # Root sentences are not rogue
             return False
 
-        ham_children = self.children.ham
-        if ham_children.count() == 0:
-            # Potential leaf
-            head = self.head
-            depth = self.depth
-            competitors = [s for s in self.tree.sentences.ham.all()
-                           if s.children.ham.count() == 0 and s.head == head]
-
-            betters = [s for s in competitors if s.depth > depth]
-            if len(betters) > 0:
-                # Another leaf (or even several) in this branch is strictly
-                # deeper
-                return True
-
-            equals = [s for s in competitors if s.depth == depth]
-            if len(equals) > 0:
-                # Another leaf (or even several) in this branch is as deep as
-                # we are. Take the sentence created earliest as non-rogue.
-                equals = sorted(equals, key=lambda s: s.created)
-                return equals[0] != self
-
-            # No better competitor, so we're not rogue
+        if self.children.nonspam.nondoublepost.count() > 0:
+            # We're not a leaf, so not self_rogue (though we could have
+            # rogue children)
             return False
-        else:
-            # We're not a leaf, so see if our children are rogue
-            return np.all([c.rogue for c in ham_children])
 
-    models.Sentence.rogue = property(get_rogue)
+        # We're a potential leaf, do we compete with other leaves in this
+        # branch?
+        head = self.head
+        depth = self.depth
+        competitors = \
+            [s for s in self.tree.sentences.nonspam.nondoublepost.all()
+             if (s.children.nonspam.nondoublepost.count() == 0
+                 and s.head == head)]
+
+        betters = [s for s in competitors if s.depth > depth]
+        if len(betters) > 0:
+            # Another leaf (or even several) in this branch is strictly
+            # deeper
+            return True
+
+        equals = [s for s in competitors if s.depth == depth]
+        if len(equals) > 0:
+            # Another leaf (or even several) in this branch is as deep as
+            # we are. Take the sentence with smallest id as non-self_rogue.
+            equals = sorted(equals, key=lambda s: s.id)
+            return equals[0] != self
+
+        # No better competitor, so we're not self_rogue
+        return False
+
+    models.Sentence._self_rogue = property(get_self_rogue)
+    models.Sentence.rogue = property(memoized(
+        lambda self: (self._self_rogue or
+                      (self.children.nonspam.nondoublepost.count() > 0 and
+                       np.all([c.rogue for c in
+                               self.children.nonspam.nondoublepost.all()])))
+    ))
+
+    # Give easy access to non-rogue sentences
+    @memoized
+    def get_nonrogue(self):
+        if self.model != models.Sentence:
+            raise ValueError('Only available on Sentence')
+
+        ids = [s.id for s in self.all() if not s.rogue]
+        return self.filter(pk__in=ids)
+
+    Manager.nonrogue = property(get_nonrogue)
+    QuerySet.nonrogue = property(get_nonrogue)
 
     # Easily access sentences we want to keep
     @memoized
@@ -323,15 +389,16 @@ def equip_sentence_codings(models):
         if self.model != models.Sentence:
             raise ValueError('Only available on Sentence')
 
-        ham = self.ham
-        ids = [s.id for s in ham if not s.rogue]
-        return ham.filter(pk__in=ids)
+        return self.nonspam.nondoublepost.nonrogue
 
     Manager.kept = property(get_kept)
+    QuerySet.kept = property(get_kept)
     Manager.with_dropped = lambda self, with_dropped: \
-        self.get_queryset() if with_dropped else self.kept
+        self if with_dropped else self.kept
+    QuerySet.with_dropped = lambda self, with_dropped: \
+        self if with_dropped else self.kept
 
-    models.Sentence.LOADED_CODINGS = ['spam', 'rogue']
+    models.Sentence.LOADED_CODINGS = ['spam', 'doublepost', 'rogue']
 
     # Test spam (hard to test, so only checking that what we entered as first
     # sentences is not spam)
@@ -351,15 +418,36 @@ def equip_sentence_codings(models):
         assert models.Sentence.objects.get(id=2608).spam
         assert not models.Sentence.objects.get(id=244).spam
 
-    # Test ham
+    # Test nonspam
+    try:
+        models.Profile.objects.nonspam
+    except ValueError:
+        pass  # Test passed
+    else:
+        raise Exception('ValueError not raised on Profile.objects.nonspam')
     if settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
-        assert models.Sentence.objects.ham.get(id=1) is not None
-        try:
-            models.Profile.objects.ham
-        except ValueError:
-            pass  # Test passed
-        else:
-            raise Exception('ValueError not raised on Profile.objects.ham')
+        assert models.Sentence.objects.nonspam.get(id=1) is not None
+    if settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert models.Sentence.objects.nonspam.get(id=6) is not None
+
+    # Test doublepost
+    if settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert models.Sentence.objects.get(id=258).doublepost
+        assert models.Sentence.objects.get(id=1557).doublepost
+        assert models.Sentence.objects.get(id=442).doublepost
+        assert models.Sentence.objects.get(id=677).doublepost
+        assert models.Sentence.objects.get(id=1453).doublepost
+
+    # Test nondoublepost
+    try:
+        models.Profile.objects.nondoublepost
+    except ValueError:
+        pass  # Test passed
+    else:
+        raise Exception('ValueError not raised on '
+                        'Profile.objects.nondoublepost')
+    if settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert models.Sentence.objects.nondoublepost.get(id=984) is not None
 
     # Test rogue
     if settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
@@ -372,9 +460,23 @@ def equip_sentence_codings(models):
         assert not models.Sentence.objects.get(id=1452).rogue
         assert models.Sentence.objects.get(id=1453).rogue
 
+    # Test nonrogue
+    try:
+        models.Profile.objects.nonrogue
+    except ValueError:
+        pass  # Test passed
+    else:
+        raise Exception('ValueError not raised on '
+                        'Profile.objects.nonrogue')
+    if settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert models.Sentence.objects.nonrogue.get(id=984) is not None
+
     # Test kept
     if settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
         assert models.Sentence.objects.kept.get(id=1) is not None
+    if settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert models.Sentence.objects.kept.get(id=6) is not None
+        assert models.Sentence.objects.kept.get(id=1618) is not None
 
     # Test with_dropped
     if settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
@@ -409,7 +511,8 @@ def equip_profile_transformation_rate(models):
                     distance_type, models.Sentence.DISTANCE_TYPES))
         distance_name = distance_type + '_distance'
 
-        sentences = self.sentences if with_spam else self.sentences.ham
+        sentences = (self.sentences.nondoublepost if with_spam
+                     else self.sentences.nondoublepost.nonspam)
         transformed_sentences = sentences.filter(parent__isnull=False).all()
         if len(transformed_sentences) == 0:
             raise ValueError("Profile has no reformulated sentences "
