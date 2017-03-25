8a7d05e10b2a3a49d1b20244a4f678e1fb4d8b77
Sébastien Lerique <sl@mehho.net>
2017-03-25 16:31:59 +0100

    chore/structure: update settings

--- analysis/settings.py
+++ analysis/settings.py
@@ -1,12 +1,12 @@
 from os.path import join, abspath, dirname
 
 
-settings_folder = dirname(abspath(__file__))
+base_folder = dirname(dirname(abspath(__file__)))
 
-MODELS_FOLDER = join(settings_folder, 'models')
-MODEL_TEMPLATE = join(MODELS_FOLDER, '{n}-gram_{type}.pickle')
-
-DATA_FOLDER = join(settings_folder, 'data')
+DATA_FOLDER = join(base_folder, 'data')
 AOA = join(DATA_FOLDER, 'Kuperman-BRM-data-2012.csv')
 CLEARPOND = join(DATA_FOLDER, 'englishCPdatabase2.txt')
 FREQUENCY = join(DATA_FOLDER, 'SUBTLEX-UK.txt')
+
+MODELS_FOLDER = join(DATA_FOLDER, 'models')
+MODEL_TEMPLATE = join(MODELS_FOLDER, '{n}-gram_{type}.pickle')
