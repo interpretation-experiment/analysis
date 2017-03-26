ee81ecfa5b421a9b80d8072521d861a121657c1c
Sébastien Lerique <sl@mehho.net>
2017-03-26 18:00:46 +0200

    test/contents: update for exp_2

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -11,7 +11,7 @@ def setup(db_name):
     shaping.equip_model_managers_with_bucket_type(models)
     shaping.equip_sentence_with_head_depth(models)
 
-    contents.equip_sentence_content_words(models)
+    contents.equip_sentence_words(models)
     contents.equip_sentence_distances(models)
     contents.equip_profile_transformation_rate(models)
 
--- analysis/contents_test.py
+++ analysis/contents_test.py
@@ -1,18 +1,41 @@
-import numpy as np
+from numpy.testing import assert_approx_equal
 
 
-# TODO: refresh tests
-
-
-def test_equip_sentence_content_words(models):
+def test_equip_sentence_words(models):
     from django.conf import settings as django_settings
 
     # Test
-    if django_settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
+    if django_settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert models.Sentence.objects.get(id=1).words == \
+            ("it", "was", "that", "night", "i", "discovered", "that", "most",
+             "things", "you", "consider", "evil", "or", "wicked", "are",
+             "simply", "lonely", "and", "lacking", "in", "the", "social",
+             "niceties")
+        assert models.Sentence.objects.get(id=2).words == \
+            ("see", "most", "men", "they", "'ll", "tell", "a", "story",
+             "straight", "through", "and", "it", "wo", "n't", "be",
+             "complicated", "but", "it", "wo", "n't", "be", "interesting",
+             "either")
         assert models.Sentence.objects.get(id=1).content_words == \
-            ['young', 'boy', 'sudden', 'hit', 'littl', 'girl']
+            ('night', 'discovered', 'things', 'consider', 'evil', 'wicked',
+             'simply', 'lonely', 'lacking', 'social', 'niceties')
+        # spaCy doesn't recognise "wo" in "won't" as "will" (why not? seems
+        # pretty standard), leading it to show up in content words. But this
+        # only adds a little more strictness to content word measures, which is
+        # no fundamental problem
         assert models.Sentence.objects.get(id=2).content_words == \
-            ['forget', 'leav', 'door', 'open', 'leav', 'offic']
+            ('men', 'tell', 'story', 'straight', 'wo', 'complicated', 'wo',
+             'interesting')
+        assert models.Sentence.objects.get(id=1).content_lemmas == \
+            ('night', 'discover', 'thing', 'consider', 'evil', 'wicked',
+             'simply', 'lonely', 'lack', 'social', 'nicety')
+        assert models.Sentence.objects.get(id=2).content_lemmas == \
+            ('man', 'tell', 'story', 'straight', 'wo', 'complicated', 'wo',
+             'interesting')
+        assert models.Sentence.objects.get(id=1).content_ids == \
+            (3, 5, 8, 10, 11, 13, 15, 16, 18, 21, 22)
+        assert models.Sentence.objects.get(id=2).content_ids == \
+            (2, 5, 7, 8, 12, 15, 18, 21)
 
 
 def test_equip_sentence_distances(models):
@@ -20,59 +43,71 @@ def test_equip_sentence_distances(models):
 
     # Testing distance is hard (we don't have predictable data for it),
     # so we mostly test for stupid values only
-    if django_settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
+    if django_settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
         assert models.Sentence.objects.get(id=1).raw_distance(
                 models.Sentence.objects.get(id=1)) == 0.0
-        assert models.Sentence.objects.get(id=1).ordered_content_distance(
+        assert models.Sentence.objects.get(id=1).ow_distance(
                 models.Sentence.objects.get(id=1)) == 0.0
-        assert models.Sentence.objects.get(id=1).unordered_content_distance(
+        assert models.Sentence.objects.get(id=1).oc_distance(
                 models.Sentence.objects.get(id=1)) == 0.0
-        assert np.abs(models.Sentence.objects.get(id=1).raw_distance(
-                models.Sentence.objects.get(id=2)) - .754098) <= 1e-6
-        assert models.Sentence.objects.get(id=1).ordered_content_distance(
-                models.Sentence.objects.get(id=2)) == 1.0
-        assert models.Sentence.objects.get(id=1).unordered_content_distance(
-                models.Sentence.objects.get(id=2)) == 1.0
+        assert models.Sentence.objects.get(id=1).uc_distance(
+                models.Sentence.objects.get(id=1)) == 0.0
+        assert_approx_equal(models.Sentence.objects.get(id=26)
+                            .raw_distance(models.Sentence.objects.get(id=160)),
+                            .32941176470588235)
+        assert models.Sentence.objects.get(id=26).ow_distance(
+                models.Sentence.objects.get(id=160)) == .375
+        assert_approx_equal(models.Sentence.objects.get(id=26)
+                            .oc_distance(models.Sentence.objects.get(id=160)),
+                            .16666666666666666)
+        assert_approx_equal(models.Sentence.objects.get(id=26)
+                            .uc_distance(models.Sentence.objects.get(id=160)),
+                            .16666666666666666)
 
     # cum_root_distance is also hard to test, so testing stupid values
-    if django_settings.DATABASES['default']['NAME'] == 'spreadr_exp_1':
-        assert models.Sentence.objects.get(id=580).cum_root_distance(
-            'raw') == 0
-        assert models.Sentence.objects.get(id=580).cum_root_distance(
-            'raw', normalized=False) == 0
-        assert models.Sentence.objects.get(id=580).cum_root_distance(
-            'ordered_content') == 0
-        assert models.Sentence.objects.get(id=580).cum_root_distance(
-            'ordered_content', normalized=False) == 0
-        assert models.Sentence.objects.get(id=580).cum_root_distance(
-            'unordered_content') == 0
-        assert models.Sentence.objects.get(id=580).cum_root_distance(
-            'unordered_content', normalized=False) == 0
-        assert models.Sentence.objects.get(id=823).cum_root_distance(
-            'raw') == .02
-        assert models.Sentence.objects.get(id=823).cum_root_distance(
+    if django_settings.DATABASES['default']['NAME'] == 'spreadr_exp_2':
+        assert_approx_equal(models.Sentence.objects.get(id=2060)
+                            .cum_root_distance('raw'), .012987012987012988)
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
             'raw', normalized=False) == 1
-        assert models.Sentence.objects.get(id=823).cum_root_distance(
-            'ordered_content') == 0
-        assert models.Sentence.objects.get(id=823).cum_root_distance(
-            'ordered_content', normalized=False) == 0
-        assert models.Sentence.objects.get(id=823).cum_root_distance(
-            'unordered_content') == 0
-        assert models.Sentence.objects.get(id=823).cum_root_distance(
-            'unordered_content', normalized=False) == 0
-        assert abs(models.Sentence.objects.get(id=1115).cum_root_distance(
-            'raw') - .308333) < 1e-6
-        assert models.Sentence.objects.get(id=1115).cum_root_distance(
-            'raw', normalized=False) == 15
-        assert abs(models.Sentence.objects.get(id=1115)
-                   .cum_root_distance('ordered_content') - .166666) < 1e-6
-        assert models.Sentence.objects.get(id=1115).cum_root_distance(
-            'ordered_content', normalized=False) == 1
-        assert abs(models.Sentence.objects.get(id=1115)
-                   .cum_root_distance('unordered_content') - .166666) < 1e-6
-        assert abs(models.Sentence.objects.get(id=1115)
-                   .cum_root_distance('unordered_content', normalized=False) -
-                   .166666) < 1e-6
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
+            'ow') == 0
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
+            'ow', normalized=False) == 0
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
+            'oc') == 0
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
+            'oc', normalized=False) == 0
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
+            'uc') == 0
+        assert models.Sentence.objects.get(id=2060).cum_root_distance(
+            'uc', normalized=False) == 0
+        assert_approx_equal(models.Sentence.objects.get(id=1347)
+                            .cum_root_distance('raw'), .37662337662337664)
+        assert models.Sentence.objects.get(id=1347).cum_root_distance(
+            'raw', normalized=False) == 29
+        assert models.Sentence.objects.get(id=1347).cum_root_distance(
+            'ow') == .4375
+        assert models.Sentence.objects.get(id=1347).cum_root_distance(
+            'ow', normalized=False) == 7
+        assert models.Sentence.objects.get(id=1347).cum_root_distance(
+            'oc') == 0.6
+        assert models.Sentence.objects.get(id=1347).cum_root_distance(
+            'oc', normalized=False) == 3
+        assert_approx_equal(models.Sentence.objects.get(id=1347)
+                            .cum_root_distance('uc'), .6666666666666666)
+        # UC distance is always normalised
+        assert_approx_equal(models.Sentence.objects.get(id=1347)
+                            .cum_root_distance('uc', normalized=False),
+                            .6666666666666666)
+
+    try:
+        models.Sentence.objects.get(id=1).cum_root_distance('unknown')
+    except ValueError:
+        pass  # Test passed
+    else:
+        raise Exception('Exception not raised on sentence for unknown '
+                        'cum_root_distance')
 
 
 def test_equip_profile_transformation_rate(models):
